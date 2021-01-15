ee68ab5aa14189cf00fd7ca56b6e0bab8a3dd73f
RubySakura <ruby_sakura@qq.com>
2021-01-15 17:11:44 +0800

    仅执行dblclick而不执行click的方法

--- test/index.js
+++ test/index.js
@@ -1,49 +1,17 @@
-// 创建一个支持冒泡且不能被取消的look事件
 var btn = document.querySelector("#btn");
-var out = document.querySelector("#out");
 
-out.onclick = function() {
-    console.log('out click');
-}
+var timers = [];
 
-btn.addEventListener("click", function(e) {
-    console.log("btn click 1");
+btn.addEventListener("click", function (e) {
+  var timer = setTimeout(function () {
+    console.log("btn click");
+  }, 300);
+  timers.push(timer);
 });
 
-btn.addEventListener("click", function(e) {
-    console.log("btn click 2");
-    e.stopImmediatePropagation();
+btn.addEventListener("dblclick", function (e) {
+  timers.forEach(function(item) {
+    clearTimeout(item);
+  });
+  console.log("btn dblclick");
 });
-
-btn.addEventListener("click", function(e) {
-    console.log("btn click 3");
-});
-
-
-
-
-
-// btn.addEventListener("click", function(e) {
-//     console.log("btn click 1");
-//     e.stopImmediatePropagation();
-// });
-
-// btn.addEventListener("click", function(e) {
-//     console.log("btn click 2");
-// });
-
-// btn.addEventListener("dblclick", function(e) {
-//     console.log("btn dblclick");
-// });
-
-// out1.onclick = function() {
-//     console.log('out1');
-// }
-
-// out2.onclick = function() {
-//     console.log('out2');
-// }
-
-// out1.ondblclick = function() {
-//     console.log('out1 db');
-// }
--- 面试题/仅执行dblclick而不执行click的方法.md
+++ 面试题/仅执行dblclick而不执行click的方法.md
@@ -1,5 +1,58 @@
 # 仅执行dblclick而不执行click的方法
 
+当我们同时给一个按钮绑定click和dblclick的时候：
+
+HTML:
+
+```HTML
+<button id="btn">点我</button>
+```
+
+JavaScript：
+
+```JavaScript
+var btn = document.querySelector("#btn");
+
+btn.addEventListener("click", function (e) {
+  console.log("btn click");
+});
+
+btn.addEventListener("dblclick", function (e) {
+  console.log("btn dblclick");
+});
+```
+
+双击按钮，输出如下：
+```
+btn click
+btn click
+btn dblclick
+```
+
+可以看到，click和dblclick事件都被触发了，这是因为双击(dblclick)的流程是：mousedown，mouseout，click，mousedown，mouseout，click，dblclick。
+
+如果我们想在双击的时候仅执行dblclick而不执行click的话，可以使用如下方法：
+
+```JavaScript
+var btn = document.querySelector("#btn");
+
+var timers = [];
+
+btn.addEventListener("click", function (e) {
+  var timer = setTimeout(function () {
+    console.log("btn click");
+  }, 300);
+  timers.push(timer);
+});
+
+btn.addEventListener("dblclick", function (e) {
+  timers.forEach(function(item) {
+    clearTimeout(item);
+  });
+  console.log("btn dblclick");
+});
+```
+
 ## 参考文章
 
 - [同时绑定click和double click冲突的问题](https://blog.csdn.net/liduanwh/article/details/79501903)
