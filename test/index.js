d77393d44ad37d18638cb9da83213d2399bdf284
RubySakura <ruby_sakura@qq.com>
2021-03-05 18:08:04 +0800

    手写new js实验

--- test/index.js
+++ test/index.js
@@ -1,10 +1,26 @@
 function myNew() {
   // 首先将参数变换为数组，并取出第0项，即构造函数
-  const argumentsArr = [...arguments];
+  const args = [...arguments];
+  const Constructor = args.shift();
   // 首先生成一个空白对象
   const obj = {};
   // 然后将原型链确定
-  
+  obj.__proto__ = Constructor.prototype;
+  const ret = Constructor.apply(obj, args);
+  if (typeof ret === "object") {
+    return ret;
+  } else {
+    return obj;
+  }
 }
 
-https://juejin.cn/post/6917811484898623495
+const Fun = function (name) {
+  this.name = name;
+};
+Fun.prototype.getName = function () {
+  alert(this.name);
+};
+let fun = myNew(Fun, "gim");
+fun.getName(); // gim
+
+// https://juejin.cn/post/6917811484898623495
